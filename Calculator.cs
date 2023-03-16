61aa23f8c9a0f040d82710612412cc2d874560ad
farhan <farhanlubisamrulloh@students.amikom.ac.id>
2023-03-16 14:55:15 +0700

    Menambahkan fungsi penambahan dan pengurangan

--- Calculator.cs
+++ Calculator.cs
@@ -2,8 +2,32 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-	  int a =
-	  int b = 
+	   Console.Write("Inputkan nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Inputkan nilai b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
+           
+
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
 	}
+
+        }
+        static int penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+       
+	
 	
 }
